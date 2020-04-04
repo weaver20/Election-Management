e79153ab0bf16237db4ac258d9c7247cf3ccf942
aviram-shubeili <62931783+aviram-shubeili@users.noreply.github.com>
2020-04-05 00:32:23 +0300

    initialize

---
+++ build/CMakeFiles/me.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/me.dir/main.c.obj"
+  "libme.dll.a"
+  "me.exe"
+  "me.exe.manifest"
+  "me.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/me.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main(int argc, char** argv)
+{
+    for(int i=0;i<argc;i++)
+    {
+        printf("\n%d. %s\n",i,argv[i]);
+    }   
+printf("end\n");
+return 0;
+}
