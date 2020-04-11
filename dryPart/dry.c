003555c120f1514e03334d77a8a09d74049dfb4b
aviram-shubeili <62931783+aviram-shubeili@users.noreply.github.com>
2020-04-11 23:48:02 +0300

    update dry.c add declaration of listCopyOrDestroyAll update nodeCreateOrDestroyAll update list1&list2 not empty option

--- dryPart/dry.c
+++ dryPart/dry.c
@@ -12,67 +12,103 @@
 #define DEMI_VALUE -9999
 
 struct node_t {
-  int x;
-  struct node_t* next;
+    int x;
+    struct node_t* next;
 };
 
 static void destroyList(Node ptr);
 static Node createOrDestroy(int data);
 
 int getListLength(Node list) {
-  int length = 0;
-  while (list != NULL)
-    length++;
+    int length = 0;
+    while (list != NULL)
+        length++;
 
 
 
 
 
 
-  return length - 1;  // first Node is a demi.
+    return length - 1;  // first Node is a demi.
 }
 
 // TODO:
 bool isListSorted(Node list) {
-  return true;
+    return true;
 }
 
-Node createNode(int data) {
-  Node ptr = malloc(sizeof(*ptr));
-  if (ptr == NULL) {
-    return NULL;
-  }
-  ptr->x = data;
-  ptr->next = NULL;
-  return ptr;
+Node nodeCreateOrDestroyAll(Node head, int data) {
+    Node ptr = malloc(sizeof(*ptr));
+    if (ptr == NULL) {
+        return NULL;
+    }
+    ptr->x = data;
+    ptr->next = NULL;
+    return ptr;
 }
 
 void destroyList(Node ptr) {
-  while (ptr) {
-    Node toDelete = ptr;
-    ptr = ptr->next;
-    free(toDelete);
-  }
+    while (ptr) {
+        Node toDelete = ptr;
+        ptr = ptr->next;
+        free(toDelete);
+    }
+}
+
+ErrorCode listCopyOrDestroyAll(Node list1,Node merged_ptr,Node merged_head)
+{
+
+
 }
 
 ErrorCode mergeSortedLists(Node list1, Node list2, Node* merged_out) {
-  // making sure merged_out is not NULL
-  Node node1 = list1, node2 = list2, node_out = *merged_out;
-  (*merged_out) = createNode(DEMI_VALUE);
-  if (merged_out == NULL) {
-    return MEMORY_ERROR;
-  }
-  int list1_length = getListLength(list1);
-  int list2_length = getListLength(list2);
+    // making sure merged_out is not NULL
+    Node node1 = list1, node2 = list2, merged_ptr = *merged_out;
+    merged_ptr = nodeCreateOrDestroyAll(*merged_out, DEMI_VALUE);
+    if (merged_ptr == NULL) {
+        return MEMORY_ERROR;
+    }
+    int list1_length = getListLength(list1);
+    int list2_length = getListLength(list2);
 
-  if (list1_length == EMPTY || list2_length == EMPTY) {
-    return EMPTY_LIST;  // what do we do with merged_out?
-  }
+    if (list1_length == EMPTY && list2_length == EMPTY) {
+        return EMPTY_LIST;  // what do we do with merged_out?
+    }
 
+    //if we made it so far both of the lists are not empty
+    while (list1 != NULL && list2 != NULL) {
+        int merge_from = (list1->x <= list2->x) ? 1 : 2;
+        switch (merge_from) {
+            case 1:
+                //..
+                merged_ptr->next = nodeCreateOrDestroyAll(*merged_out, list1->x);
+                if (merged_ptr->next == NULL) {
+                    return MEMORY_ERROR;
+                }
+                merged_ptr = merged_ptr->next;
+                list1 = list1->next;
+                break;
+            case 2:
+                //..
+                merged_ptr->next = nodeCreateOrDestroyAll(*merged_out, list2->x);
+                if (merged_ptr->next == NULL) {
+                    return MEMORY_ERROR;
+                }
+                merged_ptr = merged_ptr->next;
+                list2 = list2->next;
+                break;
+        }
 
-  while (list1 != NULL && list2 != NULL) {
-    int merge_from = (list1->x <= list2->x) ? 1 : 2;
-    (*merged_out)->next = createNode()
-
-  }
+    }
+        // TODO: maybe do a recursive call instead??
+    if (list1 == NULL) {
+        if (listCopyOrDestroyAll(list2, merged_ptr, *merged_out) == MEMORY_ERROR) {
+            return MEMORY_ERROR;
+        }
+    } else if (list2 == NULL) {
+        if (listCopyOrDestroyAll(list1, merged_ptr, *merged_out) == MEMORY_ERROR) {
+            return MEMORY_ERROR;
+        }
+    }
+    return SUCCESS;
 }
