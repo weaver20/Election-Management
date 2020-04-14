4469da6e2f99c63508902268049de5157bfcccbd
aviram <aviramsh@campus.technion.ac.il>
2020-04-14 14:06:01 +0300

    this

---
+++ cmake-build-debug-remote/CMakeFiles/dry.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/aviramsh/mtm/Clion/Ex-1/dryPart/dry.c" "/home/aviramsh/mtm/Clion/Ex-1/cmake-build-debug-remote/CMakeFiles/dry.dir/dryPart/dry.c.o"
+  "/home/aviramsh/mtm/Clion/Ex-1/dryPart/dryMain.c" "/home/aviramsh/mtm/Clion/Ex-1/cmake-build-debug-remote/CMakeFiles/dry.dir/dryPart/dryMain.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../dryPart"
+  "../ex1_from_server"
+  "../ex1_from_server/mtm_map"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
