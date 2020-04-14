548b4a7c8676b9a8db21ab8edf866199b4f61128
aviram <aviramsh@campus.technion.ac.il>
2020-04-14 14:04:43 +0300

    this

--- cmake-build-debug/CMakeFiles/dry.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/dry.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/dryPart/dry.c" "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/cmake-build-debug/CMakeFiles/dry.dir/dryPart/dry.c.obj"
-  "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/dryPart/dryMain.c" "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/cmake-build-debug/CMakeFiles/dry.dir/dryPart/dryMain.c.obj"
+  "C:/Ex-1/dryPart/dry.c" "C:/Ex-1/cmake-build-debug/CMakeFiles/dry.dir/dryPart/dry.c.obj"
+  "C:/Ex-1/dryPart/dryMain.c" "C:/Ex-1/cmake-build-debug/CMakeFiles/dry.dir/dryPart/dryMain.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/map.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/map.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/wetPart/map/map.c" "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/cmake-build-debug/CMakeFiles/map.dir/wetPart/map/map.c.obj"
-  "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/wetPart/map/map_example_test.c" "C:/Users/avira/OneDrive - Technion/GitHub/Ex-1/cmake-build-debug/CMakeFiles/map.dir/wetPart/map/map_example_test.c.obj"
+  "C:/Ex-1/wetPart/map/map.c" "C:/Ex-1/cmake-build-debug/CMakeFiles/map.dir/wetPart/map/map.c.obj"
+  "C:/Ex-1/wetPart/map/map_example_test.c" "C:/Ex-1/cmake-build-debug/CMakeFiles/map.dir/wetPart/map/map_example_test.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
