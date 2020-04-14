994280091de5dd70ec644701375d5fffb9cddf11
Noam_ <noamchn75@gmail.com>
2020-04-14 13:38:01 +0300

    I modified the mapPut function and a few other assistance functions.

---
+++ cmake-build-debug/CMakeFiles/map.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Noam_/CLionProjects/exercise-1/wetPart/map/map.c" "C:/Users/Noam_/CLionProjects/exercise-1/cmake-build-debug/CMakeFiles/map.dir/wetPart/map/map.c.obj"
+  "C:/Users/Noam_/CLionProjects/exercise-1/wetPart/map/map_example_test.c" "C:/Users/Noam_/CLionProjects/exercise-1/cmake-build-debug/CMakeFiles/map.dir/wetPart/map/map_example_test.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../dryPart"
+  "../ex1_from_server"
+  "../ex1_from_server/mtm_map"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
