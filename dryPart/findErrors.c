97fa08c280f13c1366b6ebc97cb31053972cc00e
aviram <aviramsh@campus.technion.ac.il>
2020-04-14 13:06:56 +0300

    add dryPart second question

--- dryPart/findErrors.c
+++ dryPart/findErrors.c
@@ -6,15 +6,45 @@
 #include <string.h>
 #include <assert.h>
 
-char *stringDuplicator(char *s, int times) { // PROGRAMMING ERROR: should be char* stringDuplicator(char *s, int times)
+
+char *stringDuplicator(char *s, int times) {
+    // CONVENTION ERROR 1: s should be str
+    // CONVENTION ERROR 2: func name should be a verb
     assert(!s);
+    // PROGRAMMING ERROR 1: should be assert(s)
     assert(times > 0);
-    int LEN = strlen(s); // CONVENTION ERROR: LEN - variable names should be in lower case
-    char *out = malloc(LEN * times); // PROGRAMMING ERROR: should be: malloc(sizeof(LEN) * times)
-    assert(out);                           // PROGRAMMING ERROR: should be if(!out) { **end with error**}
+    int LEN = strlen(s);
+    // CONVENTION ERROR 3: LEN - variable names should be in lower case
+    char *out = malloc(LEN * times);
+    assert(out);
+    // PROGRAMMING ERROR 2: should be if(!out) { return NULL; }
     for (int i = 0; i < times; i++) {
-        out = out + LEN;
-        strcpy(out, s);
+    // CONVENTION ERROR 4: no indent lines in for loop
+    out = out + LEN;
+    // PROGRAMMING ERROR 3: this two lines should flip - first copy, than increment pointer
+    strcpy(out, s);
+    }
+    return out;
+    // PROGRAMMING ERROR 4: returning a pointer to the end of out string
+}
+
+
+                    // **********  Fixed: **********
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+char* stringDuplicate(char *str, int times) {
+    assert(str);
+    assert(times > 0);
+    char* out = malloc(strlen(str) * times);
+    if(!out) {
+        return NULL;
+    }
+    for (int i = 0; i < times; i++) {
+        strcat(out, str);
     }
     return out;
 }
--- playground/playground.c
+++ playground/playground.c
@@ -11,18 +11,38 @@
 #include <assert.h>
 #include <stdio.h>
 
-char *stringDuplicator(char *s, int times) { // PROGRAMMING ERROR: should be char* stringDuplicator(char *s, int times)
-    assert(!s);
+char* stringDuplicate(char *str, int times) {
+    assert(str);
     assert(times > 0);
-    int LEN = strlen(s); // CONVENTION ERROR: LEN - variable names should be in lower case
-    char *out = malloc(LEN * times); // PROGRAMMING ERROR: should be: malloc(sizeof(LEN) * times)
-    assert(out);                           // PROGRAMMING ERROR: should be if(!out) { **end with error**}
+    char* out = malloc(strlen(str) * times);
+    if(!out) {
+        return NULL;
+    }
     for (int i = 0; i < times; i++) {
-        strcat(out, s);
+        strcat(out, str);
     }
     return out;
 }
+
+
+char *stringDuplicator(char *s, int times) { // CONVENTION ERROR 1: s should be str
+    assert(!s);                   // PROGRAMMING ERROR 1: should be assert(s)
+    assert(times > 0);
+    int LEN = strlen(s);             // CONVENTION ERROR 2: LEN - variable names should be in lower case
+    char *out = malloc(LEN * times);
+    assert(out);
+    char* tmp = out;// PROGRAMMING ERROR 2: should be if(!out) { return NULL; }
+    for (int i = 0; i < times; i++) {
+        // this line increment the pointer. -
+        strcpy(out, s);                 // PROGRAMMING ERROR 4: should use strcat().
+        out = out + LEN;                // PROGRAMMING ERROR 3: out is a pointer to char -
+    }
+    puts(tmp);
+    puts("TEST");
+    return out;
+}
+
 int main()
 {
-    puts(stringDuplicator("hey",3));
+    puts(stringDuplicate("Hey",3));
 }
