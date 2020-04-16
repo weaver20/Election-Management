c838e0668c4f97c3dc6a7361f36ec0dcd511ec29
aviram <aviramsh@campus.technion.ac.il>
2020-04-16 14:43:16 +0300

    update elections.c -add help functions

--- playground/playground.c
+++ playground/playground.c
@@ -10,12 +10,62 @@
 #include <string.h>
 #include <assert.h>
 #include <stdio.h>
-#include "C:\Ex-1\dryPart\findErrors.c"
+
+//
+// Created by aviram on 14/04/2020.
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+
+char *stringDuplicator(char *s, int times) {
+    // PROGRAMMING ERROR 1: s should be transferred to the function as const to
+    // CONVENTION ERROR 1: s should be str
+    // CONVENTION ERROR 2: func name should be a verb
+    assert(!s);
+    // PROGRAMMING ERROR 2: should be assert(s)
+    assert(times > 0);
+    int LEN = strlen(s);
+    // CONVENTION ERROR 3: LEN - variable names should be in lower case
+    // PROGRAMMING ERROR 3:we should allocate an additional bye for the /0  (strlen() returns the length of the string without the /0 char)
+    char *out = malloc(LEN * times);
+    assert(out);
+    // PROGRAMMING ERROR 4: should be if(!out) { return NULL; }
+    for (int i = 0; i < times; i++) {
+        // CONVENTION ERROR 4: no indent lines in for loop
+        out = out + LEN;
+        // PROGRAMMING ERROR 5: this two lines should flip - first copy, than increment pointer
+        strcpy(out, s);
+    }
+    return out;
+    // PROGRAMMING ERROR 6: returning a pointer to the end of out string
+}
+
+
+// **********  Fixed: **********
+
+
+char* stringDuplicate(const char *str, int times) {
+    assert(str);
+    assert(times > 0);
+    char* out = malloc(strlen(str) * (times));
+    if(!out) {
+        return NULL;
+    }
+    for (int i = 0; i < times; i++) {
+        strcat(out, str);
+    }
+    return out;
+}
+
 
 int main()
 {
     printf("\ntesting stringDuplicator: \n");
-    stringDuplicate("Hello", 3);
+    puts(stringDuplicate("Hello", 3));
+
     printf("\ntesting stringDuplicate: \n");
-    stringDuplicator("Hello", 3);
+    puts(stringDuplicator("Hello", 3));
 }
--- wetPart/elections/elections.c
+++ wetPart/elections/elections.c
@@ -5,9 +5,42 @@
 #include "election.h"
 #include <stdio.h>
 
+// TODO: tip from forum: use strtok()
 
 struct election_t {
     Map areas;
     Map tribes;
+    // todo: do we really need those?
+    int num_of_tribes;
+    int num_of_areas;
 
 };
+
+/**
+ * validate that name contains a-z and spaces only!
+ * @param name
+ * @return
+ */
+static ElectionResult validateName(const char* name);
+
+/**
+ * validate that the char is a-z or space
+ * @param toCheck
+ * @return
+ */
+static bool isValidChar(char toCheck);
+/**
+ * returns the index of the key in the map
+ * @param map
+ * @param key
+ * @return
+ *  -1 if not found
+ */
+static int getIndex(Map map, const char* key);
+/**
+ * removes ":num_of_votes_of_tribe[index]" from value string
+ * @param value
+ * @param index
+ * @return
+ */
+static ElectionResult removeVotesFromString(char* value, int index);
